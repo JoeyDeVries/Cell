0bd261ddd8facc2ef7fee068b4864c79f5049424
Jens Behley <jens.behley@igg.uni-bonn.de>
2018-03-27 11:18:19 +0200

    add const column operator

--- math/linear_algebra/matrix.h
+++ math/linear_algebra/matrix.h
@@ -1,13 +1,13 @@
 #ifndef MATH_LINEAR_ALGEBRA_MATRIX_H
 #define MATH_LINEAR_ALGEBRA_MATRIX_H
 
-#include <initializer_list>
 #include <assert.h>
+#include <initializer_list>
 
 #include "vector.h"
 
 namespace math
-{    
+{
     /* 
 
       Generic m by n dimensional matrix template type version supporting matrices of any type. The 
@@ -38,18 +38,17 @@ namespace math
     template <std::size_t m, std::size_t n, typename T>
     struct matrix
     {
-        union
-        {
+        union {
             T e[n][m];
             struct
             {
-                // allow access on a per-column basis. We do not provide support for per-row access 
+                // allow access on a per-column basis. We do not provide support for per-row access
                 // as this is not sequential in memory.
                 vector<m, T> col[n];
             };
         };
         // --------------------------------------------------------------------------------------------
-        // consturctor0: default initializes matrix to identity matrix 
+        // consturctor0: default initializes matrix to identity matrix
         matrix()
         {
             for (std::size_t col = 0; col < n; ++col)
@@ -67,20 +66,26 @@ namespace math
             assert(args.size() <= m * n);
             std::size_t cols = 0, rows = 0;
 
-			for (auto& it : args)
-			{
-				e[cols][rows++] = it;
-				if (rows >= m)
-				{
-					++cols;
-					rows = 0;
-				}
-			}
+            for (auto& it : args)
+            {
+                e[cols][rows++] = it;
+                if (rows >= m)
+                {
+                    ++cols;
+                    rows = 0;
+                }
+            }
         }
         // --------------------------------------------------------------------------------------------
-        // returns a column vector, that can again be indexed with the vector subscript operator. 
+        // returns a column vector, that can again be indexed with the vector subscript operator.
         // In effect: [][] and [] indexing is possible.
         vector<m, T>& operator[](const std::size_t colIndex)
+        {
+            assert(colIndex >= 0 && colIndex < n);
+            return col[colIndex];
+        }
+
+        const vector<m, T>& operator[](const std::size_t colIndex) const
         {
             assert(colIndex >= 0 && colIndex < n);
             return col[colIndex];
@@ -94,11 +99,10 @@ namespace math
     typedef matrix<3, 3, double> dmat3;
     typedef matrix<4, 4, double> dmat4;
 
-
     // per-matrix operations
     // --------------------------------------------------------------------------------------------
-    // addition (note that we do not define matrix scalar operations as they are not mathematically 
-    // defined; they should be defined as  operations on a matrix completely filled with the 
+    // addition (note that we do not define matrix scalar operations as they are not mathematically
+    // defined; they should be defined as  operations on a matrix completely filled with the
     // respective scalar.
     template <std::size_t m, std::size_t n, typename T>
     matrix<m, n, T> operator+(matrix<m, n, T>& lhs, matrix<m, n, T>& rhs)
@@ -130,9 +134,9 @@ namespace math
     }
     // multiplication
     // --------------------------------------------------------------------------------------------
-    // note that with matrix multiplication both matrices can have varying dimensions/sizes as long 
-    // as they adhere to the following rule: the number of columns (n) of the LHS matrix should 
-    // equal the number of rows (n) on the RHS matrix.  Theresult of the matrix multiplication is 
+    // note that with matrix multiplication both matrices can have varying dimensions/sizes as long
+    // as they adhere to the following rule: the number of columns (n) of the LHS matrix should
+    // equal the number of rows (n) on the RHS matrix.  Theresult of the matrix multiplication is
     // then always a matrix of dimensions m x o (LHS:rows x RHS:cols) dimensions.
     template <std::size_t m, std::size_t n, std::size_t o, typename T>
     matrix<m, o, T> operator*(matrix<m, n, T>& lhs, matrix<n, o, T>& rhs)
@@ -155,7 +159,7 @@ namespace math
     // multiplication with reference matrix (store directly inside provided matrix)
     // --------------------------------------------------------------------------------------------
     template <std::size_t m, std::size_t n, std::size_t o, typename T>
-    matrix<m, o, T>& mul(matrix <m, n, T> &result, const matrix<m, n, T>& lhs, const matrix<n, o, T>& rhs)
+    matrix<m, o, T>& mul(matrix<m, n, T>& result, const matrix<m, n, T>& lhs, const matrix<n, o, T>& rhs)
     {
         for (std::size_t col = 0; col < o; ++col)
         {
@@ -173,7 +177,7 @@ namespace math
     }
     // matrix * vector multiplication
     // --------------------------------------------------------------------------------------------
-    // rhs vector multiplication. We only define vector-matrix multiplication with the vector on 
+    // rhs vector multiplication. We only define vector-matrix multiplication with the vector on
     // the right-side of the equation due to the column-major convention.
     template <std::size_t m, std::size_t n, typename T>
     vector<m, T> operator*(matrix<m, n, T>& lhs, vector<n, T>& rhs)
@@ -190,5 +194,5 @@ namespace math
         }
         return result;
     }
-} 
+}
 #endif
